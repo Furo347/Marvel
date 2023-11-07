3b17b89ff7a2a332916e8b7532eeac0ea0b10872
selogurain12 <lora80123@gmail.com>
2023-11-07 15:58:25 +0100

    feat(webui/marvel): add new modification

--- marvel-app/App.tsx
+++ marvel-app/App.tsx
@@ -6,15 +6,11 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
       <Text>COUCOU</Text>
-=======
-      <Text>Hello You</Text>
       <Image
         style={styles.tinyLogo}
         source={{uri: 'https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive-960x540.jpg'}}
       />
->>>>>>> be4b0dca280a4faee3ab533269f2fd4161f6cc14
       <StatusBar style="auto" />
     </View>
   );
