7260154101701eb44e7d50bcbbf21df1b9a1738a
selogurain12 <lora80123@gmail.com>
2023-11-14 19:54:36 +0100

    feat(marvel/origin): add character

--- marvel-app/component/accueil.tsx
+++ marvel-app/component/accueil.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Menu from "./Menu";
 import { View, StyleSheet } from "react-native";
+import HomeScreenCharacteres from "./characters";
 
 export default function Accueil(){
     return (
         <View style={styles.container}>
         <Menu />
+        <HomeScreenCharacteres />
         </View>
     )
 }
