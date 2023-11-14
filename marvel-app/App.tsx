e8d0128997b00728dd9c81b6aa2d8cfd4d0452a7
Florentin Portets <florentin.portets@ynov.com>
2023-11-14 16:42:57 +0100

    Mise en place Premier Perso

--- marvel-app/App.tsx
+++ marvel-app/App.tsx
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Menu from './component/Menu';
+import Characters from './component/characters'
+
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Menu />
+      <Characters />
     </View>
   );
 }
@@ -15,6 +18,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 25,
+    marginBottom: 25,
+    marginRight: 5,
+    marginLeft: 5,
+
   },
   tinyLogo: {
     width: 500,
