f7cb0bb1b822a571f7b2f412ec0b11c07514cbf4
selogurain12 <lora80123@gmail.com>
2023-11-14 19:47:18 +0100

    feat(marvel/webui): add relation with other page

--- marvel-app/App.tsx
+++ marvel-app/App.tsx
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import Menu from './component/Menu';
+import Accueil from './component/accueil';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './component/appnavigator';
+import { createStackNavigator } from '@react-navigation/stack';
+import Comics from './component/comicsscreen';
+import Movies from './component/films';
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-
-      <Menu />
-
-=======
-      <Menu />
->>>>>>> 4b45feeb048f710aa9c652a7c5a50ae3b6607978
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Personnages" component={Accueil} />
+          <Stack.Screen name="Comics" component={Comics} />
+          <Stack.Screen name="Films" component={Movies} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
