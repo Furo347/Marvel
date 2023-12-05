0810f5352cb20b883bb87d8a28ef15f1e6ff264a
eliot guihal <eliot.guihal@ynov.com>
2023-12-05 10:29:08 +0000

    pimp my films details

--- component/filmdetails.tsx
+++ component/filmdetails.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
-import { Video } from "expo-av";
-import Constants from "expo-constants";
-import { RouteProp } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Linking } from "react-native";
+import { FontAwesome } from '@expo/vector-icons'; // Import FontAwesome icons
 import { format } from "date-fns";
 import { fr } from "date-fns/locale";
 import { Film } from "../hooks/useGetFilmByName";
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
 type RootStackParamList = {
-    FilmsDetails: { film: Film };
+  FilmsDetails: { film: Film };
 };
 
 type FilmsDetailsRouteProp = RouteProp<RootStackParamList, 'FilmsDetails'>;
@@ -22,6 +22,10 @@ const FilmsDetails = ({ route, navigation }: FilmsDetailsProps) => {
   const { film } = route.params;
   const formattedDate = format(new Date(film.release_date), "dd/MM/yyyy", { locale: fr });
 
+  const handleTrailerPress = () => {
+    Linking.openURL(film.trailer_url);
+  };
+
   return (
     <View style={styles.container}>
       <Image
@@ -31,12 +35,27 @@ const FilmsDetails = ({ route, navigation }: FilmsDetailsProps) => {
       <View style={styles.overlay}>
         <View style={styles.textContainer}>
           <Text style={styles.title}>Titre: {film.title}</Text>
-          <Text style={styles.sectionTitle}>Date de sortie: {formattedDate}</Text>
-          <Text style={styles.sectionTitle}>Réalisateur: {film.directed_by}</Text>
-          <Text style={styles.sectionTitle}>Box office: {film.box_office}</Text>
-          <Text style={styles.sectionTitle}>Durée: {film.duration}</Text>
-          <Text style={styles.sectionTitle}>Synopsis: {film.overview}</Text>
-          <Text style={styles.sectionTitle}>Lien vers la bande d'annonce: {film.trailer_url}</Text>
+          <View style={styles.iconText}>
+            <FontAwesome name="ticket" size={16} color="#FFFFFF" style={styles.icon} />
+            <Text style={styles.sectionTitle}>Box office: {film.box_office}</Text>
+          </View>
+          <View style={styles.iconText}>
+            <FontAwesome name="calendar" size={16} color="#FFFFFF" style={styles.icon} />
+            <Text style={styles.sectionTitle}>Date de sortie: {formattedDate}</Text>
+          </View>
+          <View style={styles.iconText}>
+            <FontAwesome name="pencil" size={16} color="#FFFFFF" style={styles.icon} />
+            <Text style={styles.sectionTitle}>Réalisateur: {film.directed_by}</Text>
+          </View>
+          <View style={styles.iconText}>
+            <FontAwesome name="hourglass" size={16} color="#FFFFFF" style={styles.icon} />
+            <Text style={styles.sectionTitle}>Durée: {film.duration}</Text>
+          </View>
+          <TouchableOpacity style={styles.linkContainer} onPress={handleTrailerPress}>
+            <FontAwesome name="link" size={16} color="#FFFFFF" style={styles.icon} />
+            <Text style={styles.sectionTitle}>Voir la bande d'annonce</Text>
+          </TouchableOpacity>
+          <Text style={styles.sectionTitle}>{film.overview}</Text>
           <Text style={styles.sectionTitle}>Saga: {film.saga}</Text>
           <Text style={styles.sectionTitle}>Chronologie: {film.chronology}</Text>
           <Text style={styles.sectionTitle}>Phase: {film.phase}</Text>
@@ -48,39 +67,50 @@ const FilmsDetails = ({ route, navigation }: FilmsDetailsProps) => {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
-    position: 'relative',
+    flex: 1,
   },
   image: {
-    width: '100%',
-    height: '100%',
+    flex: 1,
     resizeMode: 'cover',
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'rgba(0, 0, 0, 0.8)', // Fond sombre avec une opacité de 80%
-    display: 'flex',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)', // Reduced opacity
     justifyContent: 'center',
     alignItems: 'center',
   },
   textContainer: {
     padding: 20,
-    justifyContent: 'center',
     alignItems: 'center',
   },
   sectionTitle: {
     fontSize: 16,
     fontWeight: '300',
     color: '#FFFFFF',
-    textAlign: 'center', // Justifier le texte au centre
-    marginVertical: 5, // Espacement vertical entre les lignes
+    textAlign: 'center',
+    marginVertical: 5,
   },
   title: {
     fontSize: 24,
     fontWeight: '800',
     color: '#FFFFFF',
-    marginBottom: 10, // Espacement en bas du titre
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  iconText: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    justifyContent: 'center',
+  },
+  icon: {
+    marginRight: 5,
+  },
+  linkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
   },
 });
 
